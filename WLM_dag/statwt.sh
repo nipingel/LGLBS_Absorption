881c9e27f179858ed115fd5e0327a067c5618bba
Nickolas Pingel <nickolaspingel@dhcp-200.astro.wisc.edu>
2023-04-21 11:02:08 -0500

    run ms_transform_combine_spw

--- WLM_dag/statwt.sh
+++ WLM_dag/statwt.sh
@@ -20,7 +20,7 @@ src_name=$4
 full_path=/projects/vla-processing/measurement_sets/${src_name}/raw_measurement_sets/${ms_name}
 
 ## make call to casa
-/casa-6.5.0-15-py3.8/bin/casa -c statwt.py -n ${full_path} -v ${v_sys} -w ${v_width}
+/casa-6.5.0-15-py3.8/bin/casa --nologfile -c statwt.py -n ${full_path} -v ${v_sys} -w ${v_width}
 
 ## append ".wt" suffix to denote that these measurement sets have been re-weighted
 mv ${full_path} ${full_path}".wt"
