fa95a1d980328bb9ceef1a53f4d811561671c824
Ioana Stelea <67679444+iostelea@users.noreply.github.com>
2025-08-25 11:08:07 -0500

    Update abs_cubelet.sh

--- IC10_dag/abs_cubelet.sh
+++ IC10_dag/abs_cubelet.sh
@@ -15,10 +15,10 @@ n_chan=$5
 src_name=$6
 
 full_path=/projects/vla-processing/measurement_sets/${src_name}/${ms_name}
-output_path=/projects/vla-processing/images/${src_name}/Absorption/pipeline_rerun
+output_path=/projects/vla-processing/images/${src_name}/Absorption
 
 ## define input values
-output_name=${output_path}"/"${ra_phase_center}"_"${dec_phase_center}
+output_name=${output_path}"/"$2'_'$3
 
 # make casa call to imaging script
-casa --logfile $output_name".log" -c abs_cubelet.py -v ${full_path} -o ${output_name} -r ${ra_phase_center} -d${dec_phase_center} -n ${n_chan} -s ${start_velocity}
+/casa-6.5.0-15-py3.8/bin/casa --logfile $output_name".log" -c abs_cubelet.py -v ${full_path} -o ${output_name} -r ${ra_phase_center} -d${dec_phase_center} -n ${n_chan} -s ${start_velocity}
